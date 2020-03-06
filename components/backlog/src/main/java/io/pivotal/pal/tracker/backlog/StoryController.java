086e713fbb48310f3919a46fe849c7df70c58454
sanjunaircognizant <sanju.nair4@cognizant.com>
2020-03-06 16:09:17 +0530

    Lab12g

--- components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/StoryController.java
+++ components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/StoryController.java
@@ -45,7 +45,7 @@ public class StoryController {
 
     private boolean projectIsActive(long projectId) {
         ProjectInfo project = client.getProject(projectId);
-        return project != null && project.active;
+        return project != null ;
     }
 
     private StoryFields mapToFields(StoryForm form) {
